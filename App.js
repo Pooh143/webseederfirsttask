62899b2d4b430defaa8e83fc7b8152b19cd2d79b
poonam mishra <136369544+Pooh143@users.noreply.github.com>
2025-07-07 18:35:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TempNavbar from "./compoenets/Navbar/TempNavbar";
+
+const App = () => {
+  return (
+    <div>
+      <TempNavbar></TempNavbar>
+    </div>
+  );
+};
+
+export default App;
